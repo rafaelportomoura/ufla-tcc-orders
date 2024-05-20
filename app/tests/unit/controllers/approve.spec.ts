eefcf63d4676bb21dc68778ef5bf9fa942d5b689
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-20 10:28:18 -0300

    test: 💍 controllers/approve

---
+++ app/tests/unit/controllers/approve.spec.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import { FastifyReply } from 'fastify';
+import sinon from 'sinon';
+import { ApproveBusiness } from '../../../src/business/Approve';
+import { CODE_MESSAGES } from '../../../src/constants/codeMessages';
+import { approve } from '../../../src/controllers/approve';
+import { InternalServerError } from '../../../src/exceptions/InternalServerError';
+import { fastify_reply, fastify_request, fastify_stub } from '../../data/fastify';
+import { OrderData } from '../../data/order';
+
+describe('Controller -> GetProductBalance', async () => {
+  let business: sinon.SinonStub;
+  let res: Record<keyof FastifyReply, sinon.SinonStub>;
+  const controller = (body: Record<string, unknown>) => approve(fastify_request({ body }), fastify_reply(res));
+  beforeEach(() => {
+    sinon.restore();
+    business = sinon.stub(ApproveBusiness.prototype, 'approve');
+    res = fastify_stub();
+  });
+  it('should approve', async () => {
+    business.resolves();
+    const response = await controller({ order_id: OrderData._id() });
+    expect(res.status.args).deep.equal([[200]]);
+    expect(response).deep.equal(CODE_MESSAGES.APPROVED);
+  });
+  it('should not approve', async () => {
+    business.rejects(new Error());
+    const response = await controller({ order_id: '1' });
+    expect(res.status.args).deep.equal([[500]]);
+    expect(response).deep.equal(new InternalServerError(CODE_MESSAGES.INTERNAL_SERVER_ERROR));
+  });
+});
